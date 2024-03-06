fad6db385345d53d6362e716a343a3516ce6b0f2
Rahul23Kumar58Yadav <drkumar06884@gmail.com>
2024-03-06 07:14:30 +0530

    Ariana Grande Portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+/* ==================== Typing Animation =============================*/
+var typed = new Typed(".typing",{
+    strings:["SongWriter","Singer","Model","Influencer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
+/* ========================== Aside =================================*/
+/*const nav = document.querySelector(".nav"),
+      navList = nav.document.querySelectorAll("li"),
+      totalNavList = navList.length;
+      for(let i = 0; i<totalNavList; i++){
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click",function(){
+            for(let j = 0; j< totalNavList; j++){
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active");
+        })
+      }*/
